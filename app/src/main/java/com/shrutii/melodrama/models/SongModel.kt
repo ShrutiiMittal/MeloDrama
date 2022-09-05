b188ba20ff0d8c500289ae135d32331c87f976b5
Aman Kumar <amansingh8066@gmail.com>
2022-09-06 01:46:25 +0530

    Song list fix

--- app/src/main/java/com/shrutii/melodrama/models/SongModel.kt
+++ app/src/main/java/com/shrutii/melodrama/models/SongModel.kt
@@ -5,7 +5,7 @@ data class SongModel (
     val Song: String?=null,
     val Artist:String?=null,
     val Movie:String?=null,
-    val Released: String?=null,
+    val Released: Long?=null,
     val Genre:String?=null,
     val Links:String?=null,
     val Vibe: String?=null
