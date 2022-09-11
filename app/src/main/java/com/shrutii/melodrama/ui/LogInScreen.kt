cb67750dc7f81b8e9aeb65ba53840c69e1b2d556
Aman Kumar <amansingh8066@gmail.com>
2022-09-11 15:33:58 +0530

    UI revamp

--- app/src/main/java/com/shrutii/melodrama/ui/LogInScreen.kt
+++ app/src/main/java/com/shrutii/melodrama/ui/LogInScreen.kt
@@ -16,57 +16,51 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
 import com.shrutii.melodrama.R
+import com.shrutii.melodrama.databinding.ActivityLogInScreenBinding
 import com.shrutii.melodrama.models.UserModel
 import com.shrutii.melodrama.utils.AppSharedPref
 import com.shrutii.melodrama.utils.StringUtils
 
 class LogInScreen : AppCompatActivity() {
-    lateinit var emailEditText: EditText
-    lateinit var passwordEditText: EditText
-    lateinit var logInButton:Button
-    lateinit var signInText:TextView
+    private lateinit var binding: ActivityLogInScreenBinding
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in_screen)
-        initViews()
+        binding = ActivityLogInScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         click()
-        // Initialize Firebase Auth
         auth = Firebase.auth
     }
-    private fun initViews()
-    {
-        emailEditText= findViewById(R.id.emailid)
-        passwordEditText=findViewById(R.id.password)
-        logInButton=findViewById(R.id.button)
-        signInText=findViewById(R.id.signup)
 
-    }
-    private fun click()
-    {
-        signInText.setOnClickListener(){
-            startActivity(Intent(this,SignUp::class.java))
+    private fun click() {
+        binding.signup.setOnClickListener() {
+            startActivity(Intent(this, SignUp::class.java))
         }
-        logInButton.setOnClickListener(){
-            val isValid= checkField()
+        binding.button.setOnClickListener() {
+            val isValid = checkField()
             isValid?.let {
-                Toast.makeText(this,isValid,Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, isValid, Toast.LENGTH_SHORT).show()
             }
-                ?:run(){
-                    logInUser(emailEditText.text.toString(),passwordEditText.text.toString())
+                ?: run() {
+                    logInUser(binding.emailidTiet.text.toString(), binding.password.text.toString())
                 }
         }
+        binding.google.setOnClickListener {
+            Toast.makeText(this, "Khud se karo implement", Toast.LENGTH_LONG).show()
+        }
     }
-    private fun checkField():String? {
-        if (emailEditText.text.isNullOrBlank()) {
+
+    private fun checkField(): String? {
+        if (binding.emailidTiet.text.isNullOrBlank()) {
             return "email is null or blank"
         }
-        if (passwordEditText.text.isNullOrBlank()) {
+        if (binding.password.text.isNullOrBlank()) {
             return "password is null"
         }
         return null
     }
-    private  fun logInUser(email:String,password:String){
+
+    private fun logInUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -76,36 +70,45 @@ class LogInScreen : AppCompatActivity() {
                     user?.let {
                         getUser(user)
                     }
-                        ?:run {
-                            Toast.makeText(this,"Something went wrong",Toast.LENGTH_SHORT).show()
+                        ?: run {
+                            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
                         }
 
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("LogInScreen", "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
 
                 }
             }
 
     }
 
-    private fun getUser(user: FirebaseUser){
+    private fun getUser(user: FirebaseUser) {
         val db = Firebase.firestore
         db.collection("users").document(user.uid)
-            .get().addOnCompleteListener(){
-                if(it.isSuccessful){
-                    val map=it.result.data
+            .get().addOnCompleteListener() {
+                if (it.isSuccessful) {
+                    val map = it.result.data
                     map?.let {
-                        val userModel= UserModel(map["uid"] as String,map["emailid"] as String,map["phoneNum"] as String,map["name"]as String)
+                        val userModel = UserModel(
+                            map["uid"] as String,
+                            map["emailid"] as String,
+                            map["phoneNum"] as String,
+                            map["name"] as String
+                        )
                         AppSharedPref(this).setParam(
                             StringUtils.APP_NAME,
-                            StringUtils.IS_LOGGED_IN,true)
+                            StringUtils.IS_LOGGED_IN, true
+                        )
                         AppSharedPref(this).setParam(
                             StringUtils.APP_NAME,
-                            StringUtils.USER,userModel)
-                        startActivity(Intent(this,CategoryScreen::class.java))
+                            StringUtils.USER, userModel
+                        )
+                        startActivity(Intent(this, CategoryScreen::class.java))
                     }
 
                 }
