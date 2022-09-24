047f634ef84b4b2e818d8d145781e93203ed7435
Aman Kumar <amansingh8066@gmail.com>
2022-09-24 05:57:02 +0530

    Category screen UI fix

--- app/src/main/java/com/shrutii/melodrama/adapter/SongHorizontalListAdapter.kt
+++ app/src/main/java/com/shrutii/melodrama/adapter/SongHorizontalListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.shrutii.melodrama.R
 import com.shrutii.melodrama.databinding.ActivityCategoryScreenBinding
 import com.shrutii.melodrama.databinding.SongItemBinding
 import com.shrutii.melodrama.models.SongModel
@@ -19,13 +20,12 @@ class SongHorizontalListAdapter(
     inner class SongHorizontalListVH(val binding: SongItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(pos: Int) {
-            val temp = list[pos]?.Image?.split("/d/")?.get(1) ?: ""
-            val temp2 = temp?.split("/").get(0)
+            val temp = list[pos].Image?.split("/d/")?.get(1) ?: ""
+            val temp2 = temp.split("/")[0]
             val img = "https://drive.google.com/uc?id=$temp2"
-            //Log.e("as/log:: ", "  df  ")
-            Glide.with(context).load(img).into(binding.songImage)
+            Glide.with(context).load(img).placeholder(R.drawable.place_holder).centerCrop().into(binding.songImage)
             binding.songName.text = list[pos].Song
-
+            binding.artistName.text = list[pos].Artist
         }
     }
 
